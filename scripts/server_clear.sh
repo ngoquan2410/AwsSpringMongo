4fac05e50c8a9d35202a57b853aad4557e9fff58
qngo2410 <qngo2410@gmail.com>
2023-10-14 22:32:20 +0700

    qwea

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo service codedeploy-agent stop
-sudo service codedeploy-agent start
+#sudo service codedeploy-agent stop
+#sudo service codedeploy-agent start
 sudo rm -rf /home/ec2-user/AwsSpringMongo
