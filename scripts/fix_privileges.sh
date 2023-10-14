de47b960ef6bcd2f7c2b0e8d60f2358931e0a442
qngo2410 <qngo2410@gmail.com>
2023-10-14 16:57:47 +0700

    asda

---
+++ scripts/fix_privileges.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#chmod +x /home/ec2-user/AwsDemo/* jar
+#chmod +x /home/ec2-user/AwsDemo/server_start.sh
+#chmod +x /home/ec2-user/AwsDemo/server_stop.sh
+chmod +x ~/AwsSpringMongo/* jar
+chmod +x ~/AwsSpringMongo/scripts/server_start.sh
+chmod +x ~/AwsSpringMongo/scripts/server_stop.sh
