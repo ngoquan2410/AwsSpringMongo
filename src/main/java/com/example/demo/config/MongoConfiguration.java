bb4dc989b8f19f0b980ded711680aafa346dcce0
qngo2410 <qngo2410@gmail.com>
2023-10-17 20:17:21 +0700

    asdasd

--- src/main/java/com/example/demo/config/MongoConfiguration.java
+++ src/main/java/com/example/demo/config/MongoConfiguration.java
@@ -8,26 +8,26 @@ import org.springframework.core.env.Environment;
 
 import com.mongodb.MongoClientSettings;
 
-@Configuration
+//@Configuration
 public class MongoConfiguration {
 
-	private final MongoProperties mongoProperties;
-
-	public MongoConfiguration(MongoProperties mongoProperties) {
-		super();
-		this.mongoProperties = mongoProperties;
-	}
-
-	@Bean
-	public MongoClientSettings mongoClientSettings() {
-		return MongoClientSettings.builder()
-
-				.applyToSslSettings(builder -> builder.enabled(true).invalidHostNameAllowed(true)).build();
-	}
-
-	@Bean
-	public MongoPropertiesClientSettingsBuilderCustomizer mongoPropertiesCustomizer(MongoProperties properties,
-			Environment environment) {
-		return new MongoPropertiesClientSettingsBuilderCustomizer(properties, environment);
-	}
+//	private final MongoProperties mongoProperties;
+//
+//	public MongoConfiguration(MongoProperties mongoProperties) {
+//		super();
+//		this.mongoProperties = mongoProperties;
+//	}
+//
+//	@Bean
+//	public MongoClientSettings mongoClientSettings() {
+//		return MongoClientSettings.builder()
+//
+//				.applyToSslSettings(builder -> builder.enabled(true).invalidHostNameAllowed(true)).build();
+//	}
+//
+//	@Bean
+//	public MongoPropertiesClientSettingsBuilderCustomizer mongoPropertiesCustomizer(MongoProperties properties,
+//			Environment environment) {
+//		return new MongoPropertiesClientSettingsBuilderCustomizer(properties, environment);
+//	}
 }
