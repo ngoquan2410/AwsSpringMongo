2be130b5a8066aae40e64ae49642f32731f58586
qngo2410 <qngo2410@gmail.com>
2023-10-17 14:54:43 +0700

    sdfsd

--- scripts/ImportCertificate.sh
+++ scripts/ImportCertificate.sh
@@ -1,16 +1,11 @@
 cd ..
-cd ..
-mydir=tmp/certs
-if [ ! -e "${mydir}" ]
-then
-mkdir -p "${mydir}"
-fi
-
+cd .. 
+mydir=/tmp/certs
 truststore=${mydir}/rds-truststore.jks
-storepassword=changeit
+storepassword=<truststorePassword>
 
 curl -sS "https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem" > ${mydir}/global-bundle.pem
-split -p "-----BEGIN CERTIFICATE-----" ${mydir}/global-bundle.pem rds-ca-
+awk 'split_after == 1 {n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1}{print > "rds-ca-" n ".pem"}' < ${mydir}/global-bundle.pem
 
 for CERT in rds-ca-*; do
   alias=$(openssl x509 -noout -text -in $CERT | perl -ne 'next unless /Subject:/; s/.*(CN=|CN = )//; print')
@@ -28,4 +23,4 @@ do
    expiry=`keytool -list -v -keystore "$truststore" -storepass ${storepassword} -alias "${alias}" | grep Valid | perl -ne 'if(/until: (.*?)\n/) { print "$1\n"; }'`
    echo " Certificate ${alias} expires in '$expiry'" 
 done
-                
+                            
